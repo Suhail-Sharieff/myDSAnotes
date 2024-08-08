2eec5898a81afc337a7f59501fb64497ba1e33fb
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-08-09 00:19:37 +0530

    ''''

--- algos/test.java
+++ algos/test.java
@@ -1,28 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * test
  */
 public class test {
 
-    public static int func(int arr[],int price,int fromIdx){
-        int max=price,idxOfMax=fromIdx;
-        for (int i = fromIdx; i < arr.length; i++) {
-            if (arr[i]!=price&&arr[i]>max) {
-                max=arr[i];
-                idxOfMax=i;
-            }
-        }
-        return idxOfMax;
-    }
     public static void main(String[] args) {
-        int prices[]={7,1,5,3,6,4};
+        Scanner sc=new Scanner(System.in);
+        int  t=sc.nextInt();
+        while (t--!=0) {
+            int arr[]=new int[16];
+            int greater[]=new int[16];
+            int ans[]=new int[16];
+            List<Integer>li=new ArrayList<>();
+            for (int i = 0; i < 16; i++) {
+                arr[i]=sc.nextInt();
+                li.add(arr[i]);
+            }
 
-        int maxProfit=0;
+            Arrays.sort(arr);
+            int an2s[]=new int[16];
+            for (int i = 0; i < 16; i++) {
+                if (i==0) {
+                    an2s[li.indexOf(arr[i])]=0;
+                }
+                else if(i>=1&&i<=2){
+                    an2s[li.indexOf(arr[i])]=1;
+                }
+                else if(i>2&&i<=6){
+                    an2s[li.indexOf(arr[i])]=2;
+                }
+                else if(i>6&&i<=14){
+                    an2s[li.indexOf(arr[i])]=3;
+                }
+                else if(i>14&&i<=15){
+                    an2s[li.indexOf(arr[i])]=4;
+                }
+            }
 
-        for (int i = 0; i < prices.length; i++) {
-            int idx=func(prices, prices[i],i);
-            System.out.print(idx+" ");
+
+
+
+            for (int i : an2s) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            // for (int i : ans) {
+            //     System.out.print(i+" ");
+            // }
+            // System.out.println();
+
         }
-
 
     }
 }
