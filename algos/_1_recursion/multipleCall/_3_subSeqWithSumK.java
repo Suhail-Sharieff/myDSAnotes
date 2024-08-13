8aac4da28c3ace2aa7da37c347f20d04ea4abf65
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-08-13 18:08:20 +0530

    update sumK

--- algos/_1_recursion/multipleCall/_3_subSeqWithSumK.java
+++ algos/_1_recursion/multipleCall/_3_subSeqWithSumK.java
@@ -9,7 +9,7 @@ import java.util.Vector;
  * subSeqWithSumK
  */
 public class _3_subSeqWithSumK {
-    //func1 and func2 are for subSeq only from array and u cannot repeat arrayElemnts more than their freqency in original array/vector
+    //func1 and func2 and func5are for subSeq only from array and u cannot repeat arrayElemnts more than their freqency in original array/vector
 
     public static void func1(Vector<Integer>full,Vector<Integer>empty,int start,int targetSum){///////////TLE
         if (start>=full.size()) {
@@ -48,9 +48,25 @@ public class _3_subSeqWithSumK {
 
     
     }
+    public static void func5(int nums[],int target,List<Integer>empty,List<List<Integer>>ans,int startIdx){
+        if (target==0) {
+            ans.add(new ArrayList<>(empty));
+            return;
+        }
+        for (int i =startIdx ; i < nums.length; i++) {
+            if (i>startIdx&&nums[i]==nums[i-1]) {//handle first case where start=0 && avoid dupli also
+                continue;
+            }
+            if (nums[i]>target) {
+                break;
+            }
+            empty.add(nums[i]);
+            func5(nums, target-nums[i], empty, ans, i+1);
+        }
+    }
 
 
-    //func3 and func4 would be for same as func1 and func2 but u can repeat unlimited no if times the same elemnt from orignal array in a subsequence which would sum to given target
+    //func3 and func4 and func6would be for same as func1 and func2 but u can repeat unlimited no if times the same elemnt from orignal array in a subsequence which would sum to given target
 
     public static void func3(Integer arr[], List<Integer> empty, int start, int target, int sum, HashSet<List<Integer>> hs) {//returns unique subseq list with allowed repitions/////////////////TLE
         if (start>=arr.length) {
@@ -84,11 +100,30 @@ public class _3_subSeqWithSumK {
         }
         func4(nums, target, start+1, ans, empty);
     }
+    //IMP coz remaining may not pass all TC
+    public static void func6(int nums[],int target,List<Integer>empty,List<List<Integer>>ans,int startIdx){
+        if (target==0) {
+            ans.add(new ArrayList<>(empty));
+            return;
+        }
+        for (int i =0 ; i < nums.length; i++) {
+            if (i>startIdx&&nums[i]==nums[i-1]) {//handle first case where start=0 && avoid dupli also
+                continue;
+            }
+            if (nums[i]>target) {
+                break;
+            }
+            empty.add(nums[i]);
+            func5(nums, target-nums[i], empty, ans, i+1);
+        }
+    }
    
     public static void main(String[] args) {
         //no repitition allowed:
         // func1(new Vector<>(List.of(1,2,3,4,5,6,7,8,9,10)), new Vector<>(), 0, 5);--TLE
         func2(new Vector<>(List.of(1,2,3,4,5,6,7,8,9,10)), new Vector<>(), 0, 0 , 12);//--no TLE
+        //pass nums to func5 only after sorting
+
 
 
         //allowed same elment repetion
