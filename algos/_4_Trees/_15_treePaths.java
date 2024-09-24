1f407c3d430b40c25b327fd382eabd5c6a2e4dd6
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-09-24 20:35:39 +0530

    mod

--- algos/_4_Trees/_15_treePaths.java
+++ algos/_4_Trees/_15_treePaths.java
@@ -3,27 +3,41 @@ package _4_Trees;
 import java.util.*;
 /*
  * 
-Given the root of a binary tree, return all root-to-leaf paths in any order.
-
-A leaf is a node with no children.
-
- 
+Given a Binary Tree of nodes, you need to find all the possible paths from the root node to all the leaf nodes of the binary tree.
 
 Example 1:
 
-
-Input: root = [1,2,3,null,5]
-Output: ["1->2->5","1->3"]
+Input:
+       1
+    /     \
+   2       3
+Output: 
+["1->2", 
+"1->3"]
+Explanation: 
+All possible paths:
+[1->2,
+1->3]
 Example 2:
 
-Input: root = [1]
-Output: ["1"]
- 
+Input:
+         10
+       /    \
+      20    30
+     /  \
+    40   60
+Output: 
+["10->20->40",
+"10->20->60",
+"10->30 "]
+Your Task:
+Your task is to complete the function Paths() which takes the root node as an argument and returns all the possible paths. (All the paths are printed in new lines by the driver's code.)
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(height of the tree)
 
 Constraints:
-
-The number of nodes in the tree is in the range [1, 100].
--100 <= Node.val <= 100
+1<=n<=104
  */
 public class _15_treePaths {
     public static void main(String[] args) {
@@ -41,16 +55,16 @@ public class _15_treePaths {
         }
         //logic is to move till leaf by adding root.vals..then backtrack back by checking if any other paths
         int len=sb.length();
-        sb.append(root.val);
+        sb.append(root.val);//for list of integer,empty.add(root.val)
         if (root.left==null&&root.right==null) {
             ans.add(new String(sb));
-            return;
+            // return;    / imP NOTE: DONT ADD return HERE IN ANY CASE oR ELSE IT WOULD BACK TRACK
         }
 
         sb.append(" -> ");
         optimal(root.left, ans, sb);
         optimal(root.right, ans, sb);
-        sb.setLength(len);
+        sb.setLength(len);//for list of integer, empty.remove(empty.size()-1)
 
 
         //MISTAKE CODE://didnt reset the string builder
