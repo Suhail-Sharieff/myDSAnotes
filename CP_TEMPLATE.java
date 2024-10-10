1e37f3964909fb09d63671b4aa1110ee3e474a5f
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-10-10 16:02:16 +0530

    CP updated

--- CP_TEMPLATE.java
+++ CP_TEMPLATE.java
@@ -1,53 +1,82 @@
 import java.io.*;
 import java.util.*;
 
+
 public class CP_TEMPLATE {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-
     public static void main(String[] args) throws IOException {
-        int t = scanInt(); // number of test cases
+        int t = scanInt();
         while (t-- > 0) {
-            solve(); // solve function for each test case
+            solve();
         }
     }
 
-    public static int scanInt() throws IOException {
-        return Integer.parseInt(nextToken());
-    }
-
-    public static String scanString() throws IOException {
-        return nextToken();
-    }
-
-    public static int[] scanIntArray() throws IOException {
-        int n = scanInt(); // Read the size of the array
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++) {
-            array[i] = scanInt(); // Read each integer
-        }
-        return array;
-    }
-
-    public static int GCD(int a, int b) {
-        return (b == 0) ? (a) : GCD(b, a % b);
-    }
-
-    public static int LCM(int a, int b) {
-        return ((a * b) / GCD(a, b));
-    }
-
-    private static String nextToken() throws IOException {
-        if (st == null || !st.hasMoreTokens()) {
-            st = new StringTokenizer(br.readLine());
-        }
-        return st.nextToken();
-    }
+    
 
     // Function to solve the problem
     public static void solve() throws IOException {
-        // Your problem-solving logic goes here
+        System.out.println(getPrimeFactorsList(37));
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));static StringTokenizer st;
+    static int scanInt() throws IOException {return Integer.parseInt(nextToken());}
+    static String scanString() throws IOException {return nextToken();}
+    static int[] scanIntArray(int size) throws IOException {int[] array = new int[size];for (int i = 0; i < size; i++) {array[i] = scanInt();}return array;}
+    static int GCD(int a, int b) {return (b == 0) ? (a) : GCD(b, a % b);}
+    static int LCM(int a, int b) {return ((a * b) / GCD(a, b));}
+    static String nextToken() throws IOException {if (st == null || !st.hasMoreTokens()) {st = new StringTokenizer(br.readLine());}return st.nextToken();}
+    static List<Integer> getPrimeList(int tillWhere) {boolean isPrime[] = new boolean[tillWhere + 1];List<Integer> primesList = new ArrayList<>();Arrays.fill(isPrime, true); for (int i = 2; i <= tillWhere; i++) {if (isPrime[i]) {primesList.add(i);for (int j = i * i; j <= tillWhere; j += i) {isPrime[j] = false;}}}return primesList;}// seive of eratosthenes--O(nlog(logn))
+    static List<Integer>getDivisorList(int num){List<Integer>divisorList=new ArrayList<>();for (int i = 1; i*i <=num; i++) {if (num%i==0) {divisorList.add(i);if (num/i!=i) {divisorList.add(num/i);}}}return divisorList;}
+    static void printArray(int arr[]){for(int e:arr){System.out.print(e+" ");}System.out.println();}
+    static boolean isPrime(int n){if(n<=1){return false;}for(int i=2;i*i<=n;i++){if((n%i)==0){return false;}}return true;}
+    static List<Integer> getPrimeFactorsList(int num) {if (num <= 1) {return new ArrayList<>();}List<Integer> primefactorsList = new ArrayList<>();for (int i = 2; i*i <= num; i++){if (num%i==0){primefactorsList.add(i);while (num%i==0) {num/=i;}}}if(num!=1){primefactorsList.add(num);}return primefactorsList;}
+        
+            
+        
+        
+        
+            
+                
+                
+                    
+                
+            
+        
+        
+            
+        
+        
+    
+        
+           
+        
+        
+        
+            
+               
+            
+        
+        
+    
 }
