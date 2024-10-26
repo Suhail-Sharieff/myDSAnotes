dd24bea352d5b9309a93b0dbb9910f682377fd03
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-10-26 10:18:23 +0530

    more readbale

--- algos/_6_DynamicProgramming/_01_coinCount.java
+++ algos/_6_DynamicProgramming/_01_coinCount.java
@@ -1,4 +1,5 @@
 package _6_DynamicProgramming;
+
 //https://www.youtube.com/watch?v=H9bfqozjoqs&t=448s&ab_channel=NeetCode
 import java.util.*;
 /*
@@ -25,102 +26,100 @@ Output:
  */
 
 public class _01_coinCount {
-    
+
     public static void main(String[] args) {
         // int nOfCoins=3;
-        int target=11;
-        int arr[]={1,5,7};
-        int ans=recursive(arr, target, new boolean[target+1], new int[target+1]);
-        System.out.println((ans==Integer.MAX_VALUE)?-1:ans);
-        System.out.println(soln1(arr, target, new int[target+1]));
-        soln2(arr, target, new int[target+1], new int[target+1]);
+        int target = 11;
+        int arr[] = { 1, 5, 7 };
+        int ans = recursive(arr, target, new boolean[target + 1], new int[target + 1]);
+        System.out.println((ans == Integer.MAX_VALUE) ? -1 : ans);
+        System.out.println(soln1(arr, target, new int[target + 1]));
+        soln2(arr, target, new int[target + 1], new int[target + 1]);
 
     }
 
-   
-   
-   
-   
-    
-
-    public static void brute(int nums[],int target,int start,List<Integer>empty,int ans[]){//but still it may not pass 1 or 2 TC
-        if (start>=nums.length) {
-            if (target==0) {
-                ans[0]=Math.min(ans[0], empty.size());
+    public static void brute(int nums[], int target, int start, List<Integer> empty, int ans[]) {// but still it may not
+                                                                                                 // pass 1 or 2 TC
+        if (start >= nums.length) {
+            if (target == 0) {
+                ans[0] = Math.min(ans[0], empty.size());
             }
             return;
         }
-        if (nums[start]<=target) {
+        if (nums[start] <= target) {
             empty.add(nums[start]);
-            brute(nums, target-nums[start], start, empty,ans);
-            empty.remove(empty.size()-1);
+            brute(nums, target - nums[start], start, empty, ans);
+            empty.remove(empty.size() - 1);
         }
-        brute(nums, target, start+1,  empty,ans);
+        brute(nums, target, start + 1, empty, ans);
     }
 
+    public static int recursive(int arr[], int target, boolean ready[], int values[]) {// just memoize the recursion
+                                                                                       // O(nk),where n is the target
+                                                                                       // sum and k is the number of
+                                                                                       // coins.
 
-    public static int recursive(int arr[],int target,boolean ready[],int values[]){//just memoize the recursion O(nk),where n is the target sum and k is the number of coins.
-        
-        if (target<0) {
+        if (target < 0) {
             return Integer.MAX_VALUE;
         }
-        if (target==0) {
+        if (target == 0) {
             return 0;
         }
         if (ready[target]) {
             return values[target];
         }
-        int ans=Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
         for (int i = 0; i < arr.length; i++) {
             int subResult = recursive(arr, target - arr[i], ready, values);
-            if (subResult != Integer.MAX_VALUE) {//VIMP 
+            if (subResult != Integer.MAX_VALUE) {// VIMP
                 ans = Math.min(ans, subResult + 1); // Take one coin and add the result for the remaining target
             }
         }
-        ready[target]=true;
-        values[target]=ans;
+        ready[target] = true;
+        values[target] = ans;
         return ans;
     }
 
-    //iterative solutions
+    // iterative solutions
 
-    public static int soln1(int coins[],int target,int values[]){//o(target*nCoins)---O(target)
+    public static int soln1(int coins[], int target, int values[]) {// o(target*nCoins)---O(target)
         Arrays.fill(values, Integer.MAX_VALUE);
-        values[0] = 0;//VVVVIMP or else wrong ans
-        for (int tar = 1; tar <=target; tar++) {
+        values[0] = 0;// VVVVIMP or else wrong ans
+        for (int tar = 1; tar <= target; tar++) {
             for (int coin : coins) {
-                if (tar-coin>=0) {
-                    values[tar]=Math.min(values[tar], values[tar-coin]+1);
+                if (tar - coin >= 0) {
+                    values[tar] = Math.min(values[tar], values[tar - coin] + 1);
                 }
             }
         }
-        if (values[target]==Integer.MAX_VALUE) {
+        if (values[target] == Integer.MAX_VALUE) {
             return -1;
         }
         return values[target];
     }
 
-
-    //now suppose we also wwant what coins we r choosing
-    public static void soln2(int coins[],int target,int values[],int choosed[]){
+    // now suppose we also wwant what coins we r choosing
+    public static void soln2(int coins[], int target, int values[], int choosed[]) {
         Arrays.fill(values, Integer.MAX_VALUE);
-        values[0]=0;
-        for (int tar = 1; tar <=target; tar++) {
+        values[0] = 0;
+        for (int tar = 1; tar <= target; tar++) {
             for (int coin : coins) {
-                if (tar-coin>=0 && values[tar-coin]+1<values[tar]) {
-                    values[tar]=values[tar-coin]+1;
-                    choosed[tar]=coin;
+                if (tar - coin >= 0) {
+                    if (values[tar - coin] + 1 < values[tar]) {
+                        values[tar] = values[tar - coin] + 1;
+                        choosed[tar] = coin;
+                    }
                 }
             }
         }
-        if (values[target]==Integer.MAX_VALUE) {
+        if (values[target] == Integer.MAX_VALUE) {
             System.out.println("Not possible");
-            return ;
+            return;
         }
         System.out.println("choosed coins are:");
-        while (target>0) {
-            System.out.print(choosed[target]+" ");
-            target-=choosed[target];
+        while (target > 0) {
+            System.out.print(choosed[target] + " ");
+            target -= choosed[target];
         }
     }
 }
