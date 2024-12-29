5d576d99f71489aa6a6197948cf391b7a7733093
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-12-29 16:32:49 +0530

    exp

--- algos/_6_DynamicProgramming/_08_dearrange_balls.java
+++ algos/_6_DynamicProgramming/_08_dearrange_balls.java
@@ -32,6 +32,31 @@ Constraints:
 
 
 
+//some sample knowledge:
+/*
+Say u wanna find the no of permutations where in idx(1 based)  is not equal to 'i'. U represent this as (allPermutations-allPermutationsWhere_i_is_equal_to_idx). Here allPermutations is nothing but 'n!'. Now the challeneg is to find all permutations where idx==i. Say {c1,c2,c3} be some permutation where ci represents ith digit of permuatation of say length n,(here n=3)
+
+Now all possible permuations are : {c1,c2,c3},{c1,c3,c2},{c2,c1,c3},{c2,c3,c1},{c3,c1,c2},{c3,c2,c1}
+
+Now the number of permutations where c1 is fixed at 1st idx  are {c1,c2,c3} & {c1,c3,c2} ie 2, similarly if c2 position is fixed, we have 2 permutations and again 2 permutations for c3 fixed at 3rd position, we represnt as D(k) as the number of permutations where kth character is fixed at its corresponding index, observe that D(c1)=D(c2)=D(c3)=2 which's nothing but (3-1)!=2!=2
+
+So, in general D(ki) where k is the character kept fixed at its coreesponding index 'i', in a permuatation of length n, then number of permutations where k is fixed at i and remaining can change is (n-1)!. 
+
+This was for 1 character fixed, now suppose we keep 2 characters say c1&c2 fixed at place, we find that there's only 1 such permutation is {c1,c2,c3}, ie 1 => (3-2)!....
+
+Observe the pattern, if we keep 1 char constant its (n-1)!, 2 then (n-2)! permutations.....In general, if we keep 'x' places constant, the number of subpermutations of permutation of length n where 'x' places specified are kept constant are : (n-x)!
+
+Now lets say we have permutation {1234}, the number of permuations where idx!=i (or in other words nOfPermutations where theres no place where idx==itself) = totalNOfPermutations-[all permutations where at least there's 1 elemnt which is equal to its idx]
+
+ie n!-[{D(a)+D(b)+D(c)+D(d)}+{D(ab)+D(ac)+D(ad)+D(bc)+D(bd)+D(cd)}+D(abc)+....] = n! -({4D(a)}+{6D(ab)}+...)--using D(a)=D(b)....& D(ab)=D(bc)=D(ad).....
+= now D(a)=(n-1)! and D(ab)=(n-2)!...like that as explained previously that for 1 fixed its (n-1)!, for 2 its (n-2)!....here n=4 (coz chosen abcd)
+=> ans= n! - [{4(4-1)!} + {6(4-2)!}+....]===> now {4(4-1)! is nothing but (4!/1!)}, also {6(2!) is nothing but 4!/2!...} ie n!-[n!/1! + n!/2!+.......(-1)^n/n!---we must also include some inclusions of set]====>(n!/e) which in terms of recursion D(n)=(n-1)[D(n-2)+D(n-1)]
+=>
+
+*/
+
+
+
 public class _08_dearrange_balls {
 
     //brute force: for each permutation, check if all values satisfy idx!=a[i], if yes count such permutaions. TC n*n!---TLE
