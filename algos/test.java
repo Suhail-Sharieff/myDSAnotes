52f7e88513d352431fb8055e0135b46e70f57269
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-01-17 23:06:31 +0530

    callable

--- algos/test.java
+++ algos/test.java
@@ -1,89 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class test {
 
-
- public static void main(String[] args) {
-        int price[]={1, 10, 3, 1, 3, 1, 5, 9};//len1=>5,len2=>10
-        int lenOfRod=price.length;
-
-        // recursion(price, 0, 0, new ArrayList<>());
+    public static void main(String[] args) {
+        Temp t=new Temp();
+        Temp t2=new Temp();
+        Thread th=new Thread();
+        t.start();
+        t2.start();
     }
+}
 
-    //--------------------------recursion, i have converted preices array into 1 based indexing for clarity to call like:
-    /*
-     int len=price.length;
-        int v[]=new int [len+1];
-        for(int i=1;i<=len;i++){
-            v[i]=price[i-1];
-        }
-        return recurion(v,len,len);
-     */
-
-    public static int recursion_1(int priceOfLen[],int length_of_cut_part,int totalLength){
-        if(totalLength==0) return 0;
-        if(totalLength==1) return totalLength*priceOfLen[1];
-
-
-        int cut=(totalLength>=length_of_cut_part)?(priceOfLen[length_of_cut_part]+recursion_1(priceOfLen, length_of_cut_part, totalLength-length_of_cut_part)):0;
-        int dont_cut=recursion_1(priceOfLen, length_of_cut_part-1, totalLength);
-
-        return Math.max(cut, dont_cut);
-    }
-
-    //---------------recursion2
-    public static int recursion_2(int priceOfLen[],int totalLength){
-        if(totalLength==0) return 0;
-        if(totalLength==1) return totalLength*priceOfLen[1];
-
-        int max=0;
-        for(int chosenLen=1;chosenLen<=totalLength;chosenLen++){
-            int subRes=priceOfLen[chosenLen]+recursion_2(priceOfLen, totalLength-chosenLen);
-            max=Math.max(max, subRes);
-        }
-
-        return max;
-    }
-
-
-    //------------memoization
-    public static int memoize(int priceOfLen[],int totalLength,int dp[]){
-        if(totalLength==0) return 0;
-        if(totalLength==1) return totalLength*priceOfLen[1];
-        
-        if(dp[totalLength]!=-1) return dp[totalLength];
-
-        int max=0;
-        for(int chosenLen=1;chosenLen<=totalLength;chosenLen++){
-            int subRes=priceOfLen[chosenLen]+memoize(priceOfLen, totalLength-chosenLen,dp);
-            max=Math.max(max, subRes);
-        }
-        dp[totalLength]=max;
-
-        return max;
-    }
-
-    //----------------tabulation:
-    public static int tab(int prices[]){
-        int totalLength=prices.length;
-        int priceOfLen[]=new int[totalLength+1];//for 1 based indexing
-        for(int i=1;i<=totalLength;i++) priceOfLen[i]=prices[i-1];
-
-        int dp[]=new int[totalLength+1];
-        dp[0]=totalLength*priceOfLen[0];
-
-        for(int len=1;len<=totalLength;len++){
-            int max=0;
-            for(int chosenLen=1;chosenLen<=len;chosenLen++){
-                int subRes=priceOfLen[chosenLen]+dp[len-chosenLen];
-                max=Math.max(max, subRes);
+class Temp extends Thread{
+    @Override
+    public void run(){
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
             }
-            dp[len]=max;
         }
-
-        return dp[totalLength];
     }
-   
 }
---
+++ callable.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class callable {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Callable<Integer> someHeavyOperation=()->{
+            int k=0;
+            Thread.sleep(3000);
+            for(int i=0;i<10;i++) k=i;
+            return k;
+        };
+
+        //asssign that heavy opeartion to separate thread
+        FutureTask<Integer>task=new FutureTask<>(someHeavyOperation);
+        new Thread(task).start();
+
+        System.out.println("im executed first becoz some other thread is doint thatHeavyOperation");
+
+        int result=task.get();
+
+        System.out.println(result);
+        
+        
+    }
+}
