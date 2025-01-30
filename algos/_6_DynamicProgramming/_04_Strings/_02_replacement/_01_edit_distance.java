b65ad42a5791ba0191093f7f510f18dad5b0a686
SUHAIL SHARIEFF <149879419+Suhail-Sharieff@users.noreply.github.com>
2025-01-30 23:29:41 +0530

    Update _01_edit_distance.java

--- algos/_6_DynamicProgramming/_04_Strings/_02_replacement/_01_edit_distance.java
+++ algos/_6_DynamicProgramming/_04_Strings/_02_replacement/_01_edit_distance.java
@@ -209,8 +209,8 @@ e:[5, 4, 4, 3]
         int dp[][]=new int[len1+1][len2+1];
         for(int i=1;i<=len1;i++){
             for(int j=1;j<=len2;j++){
-                dp[i][0]=j;
-                dp[0][j]=i;
+                dp[i][0]=i;
+                dp[0][j]=j;
                 if(x.charAt(i-1)==y.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1];
                 }else{
