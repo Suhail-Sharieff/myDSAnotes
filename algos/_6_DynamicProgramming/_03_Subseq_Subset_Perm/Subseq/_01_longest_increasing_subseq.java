8f2a430c4456fdb15275c28a0fcc6d3bbb6a9d5a
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-02-11 22:22:03 +0530

    tmr

--- algos/_6_DynamicProgramming/_03_Subseq_Subset_Perm/Subseq/_01_longest_increasing_subseq.java
+++ algos/_6_DynamicProgramming/_03_Subseq_Subset_Perm/Subseq/_01_longest_increasing_subseq.java
@@ -1,7 +1,7 @@
 package _6_DynamicProgramming._03_Subseq_Subset_Perm.Subseq;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /*
@@ -36,28 +36,52 @@ Follow up: Can you come up with an algorithm that runs in O(n log(n)) time compl
  */
 public class _01_longest_increasing_subseq {
     public static void main(String[] args) {
-        int nums[]={10,9,2,5,3,7,101,18};
-        rec(nums, 0, new ArrayList<>(),0);
+        int nums[] = { 10, 9, 2, 5, 3, 7, 101, 18 };
+        test(nums, 0, new ArrayList<>(), Integer.MIN_VALUE);
     }
 
-    static void rec(int nums[],int i,List<Integer>em,int cnt){
-        if(i>=nums.length) {
-            System.out.println(new ArrayList<>(em)+" "+cnt);
-            cnt=0;
+    static void test(int nums[], int i, List<Integer> empty,  int prev) {
+        if (i >= nums.length) {
+            System.out.println(empty);
             return;
         }
-        int prev=nums[i];
-        em.add(nums[i]);
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[j]>prev){
-                em.add(nums[j]);
-                prev=nums[j];
-                rec(nums, j+1, em, cnt);
-                em.remove(em.size()-1);
-                rec(nums, j+1, em, cnt);
-            }
+        //pick logic
+        if(prev==Integer.MIN_VALUE || nums[i]>prev){
+            empty.add(nums[i]);
+            test(nums, i+1, empty, nums[i]);
+            empty.remove(empty.size()-1);
         }
-        em.remove(em.size()-1);
-        rec(nums, i+1, em, cnt);
+        test(nums, i+1, empty, prev);
+    }
+
+
+    //----------------recurive:2^n
+    public int rec(int nums[],int i,int prev_idx){
+        if(i>=nums.length-1) return 0;
+        int pick_curr=(prev_idx==-1 || nums[i]>nums[prev_idx])?1+rec(nums, i+1, i):0;
+        int dont_pick_curr=rec(nums, i+1, prev_idx);
+        return Math.max(pick_curr, dont_pick_curr);
+    }
+
+
+    //----------------brute memoizaton(coz see that prev_idx we r passing as negative)---TLE 
+    public int brute_mem(int nums[],int i,int prev_idx,HashMap<String,Integer>dp){
+        if(i>=nums.length) return 0;
+        if(dp.containsKey(i+"|"+prev_idx)) return dp.get(i+"|"+prev_idx);
+        int pick_curr=(prev_idx==-1 || nums[i]>nums[prev_idx])?1+brute_mem(nums, i+1, i,dp):0;
+        int dont_pick_curr=brute_mem(nums, i+1, prev_idx,dp);
+        dp.put(i+"|"+prev_idx,Math.max(pick_curr, dont_pick_curr));
+        return dp.get(i+"|"+prev_idx);
+    }
+
+
+    //------------------smart mem,teaches how u can hadle -ve idx as well, since u kow that prev_idx can vary from -1 to n ie only 1 negative value,instaed of storing precomputed value at dp[idx][prev_idx], store at dp[idx][prev_idx+1], solves preoblem of prev_idx being -ve
+    public int mem(int nums[],int i,int prev_idx,int dp[][]){//)(n^2)
+        if(i>=nums.length) return 0;
+        if(dp[i][prev_idx+1]!=-1) return dp[i][prev_idx+1];
+        int pick_curr=(prev_idx==-1 || nums[i]>nums[prev_idx])?1+mem(nums, i+1, i,dp):0;
+        int dont_pick_curr=mem(nums, i+1, prev_idx,dp);
+        dp[i][prev_idx+1]=Math.max(pick_curr, dont_pick_curr);
+        return dp[i][prev_idx+1];
     }
 }
