44e9ab0d133eb8e5eafe03aee3c08e84f236a238
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-02-24 21:54:22 +0530

    rerel

--- algos/_9_Linked_List/_14_merge_LL.java
+++ algos/_9_Linked_List/_14_merge_LL.java
@@ -1,5 +1,7 @@
 package _9_Linked_List;
 
+import java.util.PriorityQueue;
+
 public class _14_merge_LL {
 
     //------O(N)---O(N)
@@ -54,6 +56,65 @@ public class _14_merge_LL {
         //by this point any one of l1 or l2 must have become null
         if(l1!=null) ptr.next=l1;
         if(l2!=null) ptr.next=l2;
+        return ans.next;
+    }
+
+
+
+    //-----------------------FOLLOW UP:
+    /*
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+
+     */
+
+     //brute force can be while(k--) head=merge(head,next_list)--O(NK)--O(1)
+
+
+     public ListNode optimal(ListNode[] lists) {//O(N*KLogK)---O(K)
+        PriorityQueue<ListNode>pq=new PriorityQueue<>((x,y)->x.val-y.val);
+        for(var e:lists) if(e!=null) pq.offer(e);
+        ListNode ans=new ListNode(-1),ptr=ans;
+        while(!pq.isEmpty()){//K
+            ListNode top=pq.poll();
+            ptr.next=top;
+            ptr=ptr.next;
+            if(top.next!=null)pq.offer(top.next);//LogK
+        }
         return ans.next;
     }
 }
