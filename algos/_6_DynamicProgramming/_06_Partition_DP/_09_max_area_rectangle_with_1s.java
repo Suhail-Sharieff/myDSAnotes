5082e4a071cf64f6a87adb89f2317ce2c6cea8bf
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-02-27 23:20:53 +0530

    done

---
+++ algos/_6_DynamicProgramming/_06_Partition_DP/_09_max_area_rectangle_with_1s.java
@@ -0,0 +1,5 @@
+package _6_DynamicProgramming._06_Partition_DP;
+
+public class _09_max_area_rectangle_with_1s {
+    //https://leetcode.com/problems/max-area-of-island/description/ was about find largest island, no matter it is of whechever shape, but here specifically gievn that it should be rectangle, hence not a graph problem
+}
