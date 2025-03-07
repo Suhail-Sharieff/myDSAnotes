e98b1acf6bea0931dea2f5eaad99aa9842ecf6c6
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-03-07 15:21:35 +0530

    one more

--- algos/_10_Sliding_Window/_02_dynamic_window/_01_longest_subarray_with_sumK.java
+++ algos/_10_Sliding_Window/_02_dynamic_window/_01_longest_subarray_with_sumK.java
@@ -205,5 +205,62 @@ The space complexity is O(1) because the algorithm uses a constant amount of spa
         return totalCount;
 
     }
+
+
+
+    //-------------FOLLOW UP:
+    /*
+1248. Count Number of Nice Subarrays
+
+Hint
+Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+Example 2:
+
+Input: nums = [2,4,6], k = 1
+Output: 0
+Explanation: There are no odd numbers in the array.
+Example 3:
+
+Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+Output: 16
+ 
+
+Constraints:
+
+1 <= nums.length <= 50000
+1 <= nums[i] <= 10^5
+1 <= k <= nums.length
+     */
+    public int numberOfSubarrays(int[] nums, int k) {
+        return func(nums,k)-func(nums,k-1);
+    }
+
+    public int func(int[] nums, int k){
+        int nOdd=0;
+        int cnt=0;
+        int left_ptr=0,right_ptr=0;
+        while(right_ptr<nums.length){
+            if((nums[right_ptr]&1)!=0){
+                nOdd++;
+            }
+            while(left_ptr<=right_ptr && nOdd>k){
+                if((nums[left_ptr]&1)!=0) nOdd--;
+                left_ptr++;
+            }
+            cnt+=right_ptr-left_ptr+1;
+            right_ptr++;
+        }
+        return cnt;
+    }
 }
 
