174f50d2d5048d8be8e482571c1c1c74eecb6dea
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-03-20 14:45:42 +0530

    done

---
+++ algos/_13_Stack_Queue/_04_prev_small.java
@@ -0,0 +1,61 @@
+package _13_Stack_Queue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+/*
+Given an array a of integers of length n, find the nearest smaller number for every element such that the smaller element is on left side.If no small element present on the left print -1.
+
+Example 1:
+
+Input: n = 3
+a = {1, 6, 2}
+Output: -1 1 1
+Explaination: There is no number at the 
+left of 1. Smaller number than 6 and 2 is 1.
+Example 2:
+
+Input: n = 6
+a = {1, 5, 0, 3, 4, 5}
+Output: -1 1 -1 0 3 4
+Explaination: Upto 3 it is easy to see 
+the smaller numbers. But for 4 the smaller 
+numbers are 1, 0 and 3. But among them 3 
+is closest. Similary for 5 it is 4.
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function leftSmaller() which takes n and a as input parameters and returns the list of smaller numbers.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+1 ≤ n ≤ 10000
+0 ≤ a[i] ≤ 104  
+
+
+ */
+public class _04_prev_small {
+    static List<Integer> leftSmaller(int n, int arr[])
+    {
+        //code here
+         Stack<Integer>st=new Stack<>();
+        // int prev_smallest[]=new int[arr.length];
+        List<Integer> prev_smallest = new ArrayList<>(Collections.nCopies(n, -1));
+
+        // Arrays.fill(prev_smallest, -1);
+        for(int i=0;i<arr.length;i++){
+            while(!st.isEmpty()){
+                if(arr[i]>st.peek()){
+                    prev_smallest.set(i,st.peek());
+                    break;
+                }
+                st.pop();
+            }
+            st.push(arr[i]);
+        }
+        // System.out.println(Arrays.toString(prev_smallest));
+        return prev_smallest;
+    }
+}
---
+++ algos/_13_Stack_Queue/_05_trapping_rain_water.java
@@ -0,0 +1,123 @@
+package _13_Stack_Queue;
+
+
+/*
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+Example 1:
+
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+ 
+
+Constraints:
+
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105
+
+ */
+
+
+public class _05_trapping_rain_water {
+    //brute force:just sum up the amount of water that u can store on terace of each buiding
+    public int brute(int[] nums) {
+        int[] pref=get_pref_max(nums);
+        int[] suff=get_suff_max(nums);
+        int ans=0;
+
+        for(int i=0;i<nums.length;i++){
+            int water_can_be_stored_only_above_that_building=Math.min(pref[i],suff[i])-nums[i];
+            ans+=water_can_be_stored_only_above_that_building;
+        }
+
+        return ans;
+    }
+
+    public int[] get_pref_max(int nums[]){
+        int pref[]=new int[nums.length];
+        pref[0]=nums[0];
+        for(int i=1;i<nums.length;i++) pref[i]=Math.max(pref[i-1],nums[i]);
+        return pref;
+    }
+    public int[] get_suff_max(int nums[]){
+        int suff[]=new int[nums.length];
+        suff[nums.length-1]=nums[nums.length-1];
+        for(int i=nums.length-2;i>=0;i--) suff[i]=Math.max(suff[i+1],nums[i]);
+        return suff;
+    }
+
+
+
+    //-----------------optimal
+    //https://www.youtube.com/watch?v=ZI2z5pq0TqA&ab_channel=NeetCode
+    /*
+A basic idea comes into peoples mind if two walls are there and we want want to fill water within that wall we just have to fill the water till min of two wall right if it exceeds the value ,it will overflow.
+
+So, we can set 2 pointers left_max,right_max and check which side is greater,if one side is greater iterate on other side because at the min side the water will be filled at its limit. and iterate till pointer is lower than the other one.
+
+ex.height = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+left_max=0,right_max=1,count=0
+amount of water is calculate using subtractring wall length with min(right_max,left_max);
+
+iterate left_side.
+count=0,left_max=0,right_max=1
+
+iterate left_side.
+count=0,left_max=1,right_max=1
+
+iterate left_side.
+count=1,left_max=1,right_max=1
+
+iterate left_side.
+count=2,left_max=2,right_max=1
+
+iterate right_side.
+count=2,left_side=2,right_side=2
+
+iterate left_side.
+count=3,left_side=2,right_side=2
+
+iterate left_side.
+count=5,left_side=2,right_side=2
+
+iterate left_side.
+count=5,left_side=3,right_side=2
+
+iterate right_side.
+count=6,left_side=3,right_side=2
+
+iterate right_side.
+count=6,left_side=3,right_side=2
+
+so thats the amount of water is found.
+     */
+    public int trap(int[] nums) {
+        int ans=0;
+        int l_max=nums[0],r_max=nums[nums.length-1];
+        int l_ptr=0,r_ptr=nums.length-1;
+
+        while(l_ptr<r_ptr){
+            if(l_max<=r_max){
+                ans+=(l_max-nums[l_ptr]);
+                l_ptr++;
+                l_max=Math.max(l_max,nums[l_ptr]);
+            }else{
+                ans+=(r_max-nums[r_ptr]);
+                r_ptr--;
+                r_max=Math.max(r_max,nums[r_ptr]);
+            }
+        }
+        return ans;
+    }
+
+}
