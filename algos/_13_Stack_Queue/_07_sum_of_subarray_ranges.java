4c1a6c5bf73934dd73b5b61a155424eab5a29a5b
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-03-22 19:17:19 +0530

    done

--- algos/_13_Stack_Queue/_07_sum_of_subarray_ranges.java
+++ algos/_13_Stack_Queue/_07_sum_of_subarray_ranges.java
@@ -45,6 +45,8 @@ Constraints:
 -109 <= nums[i] <= 109
  */
 
+import java.util.Stack;
+
 public class _07_sum_of_subarray_ranges {
     public long brute(int[] nums) {
         long ans=0l;
@@ -60,5 +62,76 @@ public class _07_sum_of_subarray_ranges {
         }
         return ans;
     }
+
+
+
+    //--------------optimal:O(5n)
+
+    //since we need to find sum(max-min) for each subarray, this can also be written as [sum(max)-sum(min)] for each subarray, prev_file u know how to calacute sum(min), here additionally we find sum(max), ans is thier differnec
+     public long subArrayRanges(int[] nums) {
+        int [] previous_smaller_or_equal_idx=previous_smaller_or_equal(nums);
+        int [] next_smaller_idx=next_smaller(nums);
+
+
+        int [] previous_larger_or_equal_idx=previous_larger_or_equal(nums);
+        int [] next_larger_idx=next_larger(nums);
+
+        long sum_of_mins=0l,sum_of_maxs=0l;
+
+        for(int i=0;i<nums.length;i++){
+            sum_of_mins+=(i-previous_smaller_or_equal_idx[i])*1l*(next_smaller_idx[i]-i)*nums[i];
+            sum_of_maxs+=(i-previous_larger_or_equal_idx[i])*1l*(next_larger_idx[i]-i)*nums[i];
+        }
+        return  sum_of_maxs-sum_of_mins;
+     }
+
+     public static int[] previous_smaller_or_equal(int nums[]){
+        Stack<int[]>st=new Stack<>();
+        int previous_smaller_or_equal[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            while (!st.isEmpty() && st.peek()[0]>nums[i]) st.pop();
+            if(st.isEmpty()) previous_smaller_or_equal[i]=-1;
+            else previous_smaller_or_equal[i]=st.peek()[1];
+            st.push(new int[]{nums[i],i});
+        }
+        return previous_smaller_or_equal;
+     }
+
+
+     public static int[] next_smaller(int nums[]){
+        Stack<int[]>st=new Stack<>();
+        int next_smaller[]=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()[0]>=nums[i]) st.pop();
+            if(st.isEmpty()) next_smaller[i]=nums.length;
+            else next_smaller[i]=st.peek()[1];
+            st.push(new int[]{nums[i],i});
+        }
+        return next_smaller;
+     }
+
+    public static int[] previous_larger_or_equal(int nums[]){
+        Stack<int[]>st=new Stack<>();
+        int prev_larger[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            while(!st.isEmpty() && st.peek()[0]<nums[i]) st.pop();
+            if(st.isEmpty()) prev_larger[i]=-1;
+            else prev_larger[i]=st.peek()[1];
+            st.push(new int[]{nums[i],i});
+        }
+        return prev_larger;
+    }
+    public static int[] next_larger(int nums[]){
+        Stack<int[]>st=new Stack<>();
+        int next_larger[]=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()[0]<=nums[i]) st.pop();
+            if(st.isEmpty()) next_larger[i]=nums.length;
+            else next_larger[i]=st.peek()[1];
+            st.push(new int[]{nums[i],i});
+        }
+        return next_larger;
+     }
+
 
 }
