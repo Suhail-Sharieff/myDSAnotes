e58e0174a152a078d534a415608e4431bb43317b
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-03-22 20:33:22 +0530

    graph solved

--- algos/_7_Graph/_00_Matrix/_01_BFS_DFS/_2_count_Islands.java
+++ algos/_7_Graph/_00_Matrix/_01_BFS_DFS/_2_count_Islands.java
@@ -1,6 +1,8 @@
 package _7_Graph._00_Matrix._01_BFS_DFS;
+import java.util.ArrayList;
 //shows how we can spread from (i,j) in a matrix to its neighbours
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 /*
 Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
@@ -145,5 +147,97 @@ class Cordinate{
      Cordinate(int x,int y){
         this.x=x;
         this.y=y;
+    }
+
+
+
+
+
+    //----------------------------FOLLOW UP:
+    /*
+You are given an integer n. There is an undirected graph with n vertices, numbered from 0 to n - 1. You are given a 2D integer array edges where edges[i] = [ai, bi] denotes that there exists an undirected edge connecting vertices ai and bi.
+
+Return the number of complete connected components of the graph.
+
+A connected component is a subgraph of a graph in which there exists a path between any two vertices, and no vertex of the subgraph shares an edge with a vertex outside of the subgraph.
+
+A connected component is said to be complete if there exists an edge between every pair of its vertices.
+
+ 
+
+Example 1:
+
+
+
+Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4]]
+Output: 3
+Explanation: From the picture above, one can see that all of the components of this graph are complete.
+Example 2:
+
+
+
+Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4],[3,5]]
+Output: 1
+Explanation: The component containing vertices 0, 1, and 2 is complete since there is an edge between every pair of two vertices. On the other hand, the component containing vertices 3, 4, and 5 is not complete since there is no edge between vertices 4 and 5. Thus, the number of complete components in this graph is 1.
+ 
+
+Constraints:
+
+1 <= n <= 50
+0 <= edges.length <= n * (n - 1) / 2
+edges[i].length == 2
+0 <= ai, bi <= n - 1
+ai != bi
+There are no repeated edges.
+     */
+
+        //solution: A graph is complete if and only if the number of neighbourers of each node is equal to (totalNumberOfNodesInThatComponent-1);
+
+    public int optimal(int n, int[][] edges) {
+        List<List<Integer>>adj=get_adj(edges,n);
+        int ans=0;
+        boolean isVis[]=new boolean[n];
+        for(int nodeNumber=0;nodeNumber<n;nodeNumber++){
+            if(!isVis[nodeNumber]){
+                int nNodes_in_this_comp=dfs(nodeNumber,adj,isVis);
+                if(isComplete(adj,nNodes_in_this_comp-1,nodeNumber,new boolean[n])) ans++;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isComplete(List<List<Integer>>adj,int req_no_of_neighbours,int src,boolean isVis[]){
+        if(adj.get(src).size()!=req_no_of_neighbours) return false;
+       isVis[src]=true;
+       for(int neigh:adj.get(src)){
+           if(!isVis[neigh]){
+             boolean curr=isComplete(adj,req_no_of_neighbours,neigh,isVis);
+            if(!curr) return false;
+           }
+       }
+       return true;
+    }
+
+    public int dfs(int nodeNumber,List<List<Integer>>adj,boolean isVis[]){
+        isVis[nodeNumber]=true;
+        int cnt=1;
+        for(int neigh:adj.get(nodeNumber)){
+            if(!isVis[neigh]){
+                // System.out.print(neigh+"->");
+                cnt+=dfs(neigh,adj,isVis);
+            }
+        }        
+        return cnt;
+    }
+
+
+    public List<List<Integer>>get_adj(int edges[][],int n){
+        List<List<Integer>>adj=new ArrayList<>();
+        while(n--!=0) adj.add(new ArrayList<>());
+        for(int e[]:edges){
+             adj.get(e[0]).add(e[1]);
+             adj.get(e[1]).add(e[0]);
+        }
+        return adj;
     }
 }
