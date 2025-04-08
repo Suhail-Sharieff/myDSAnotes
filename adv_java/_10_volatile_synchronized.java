b0c3c7f4eeaa6b6f40b735a0b930bace002fb93f
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-04-08 14:53:21 +0530

    fizz buzz

--- adv_java/_10_volatile_synchronized.java
+++ adv_java/_10_volatile_synchronized.java
@@ -1,6 +1,7 @@
 package adv_java;
 
 import java.time.Duration;
+import java.util.function.IntConsumer;
 
 /*
 In Java, volatile and synchronized are both used for managing concurrency, but they serve different purposes:
@@ -119,5 +120,158 @@ class Using_Volatile {
                 Thread.sleep(Duration.ofNanos(1));
             printThird.run();
         }
+    }
+}
+
+// =================FOLLOW UP:
+// Fizz Buzz Multithread Leetcode 1195
+class FizzBuzz {
+    private int n;
+    private int curr = 1;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public synchronized void fizz(Runnable printFizz) throws InterruptedException {
+        while (curr <= n) {
+            if (curr % 3 == 0 && curr % 5 != 0) {
+                printFizz.run();
+                curr++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public synchronized void buzz(Runnable printBuzz) throws InterruptedException {
+        while (curr <= n) {
+            if (curr % 3 != 0 && curr % 5 == 0) {
+                printBuzz.run();
+                curr++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public synchronized void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (curr <= n) {
+            if (curr % 15 == 0) {
+                printFizzBuzz.run();
+                curr++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public synchronized void number(IntConsumer printNumber) throws InterruptedException {
+        while (curr <= n) {
+            if (curr % 3 != 0 && curr % 5 != 0) {
+                printNumber.accept(curr);
+                curr++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+}
+
+
+/*
+1195. Fizz Buzz Multithreaded
+Solved
+Medium
+Topics
+Companies
+You have the four functions:
+
+printFizz that prints the word "fizz" to the console,
+printBuzz that prints the word "buzz" to the console,
+printFizzBuzz that prints the word "fizzbuzz" to the console, and
+printNumber that prints a given integer to the console.
+You are given an instance of the class FizzBuzz that has four functions: fizz, buzz, fizzbuzz and number. The same instance of FizzBuzz will be passed to four different threads:
+
+Thread A: calls fizz() that should output the word "fizz".
+Thread B: calls buzz() that should output the word "buzz".
+Thread C: calls fizzbuzz() that should output the word "fizzbuzz".
+Thread D: calls number() that should only output the integers.
+Modify the given class to output the series [1, 2, "fizz", 4, "buzz", ...] where the ith token (1-indexed) of the series is:
+
+"fizzbuzz" if i is divisible by 3 and 5,
+"fizz" if i is divisible by 3 and not 5,
+"buzz" if i is divisible by 5 and not 3, or
+i if i is not divisible by 3 or 5.
+Implement the FizzBuzz class:
+
+FizzBuzz(int n) Initializes the object with the number n that represents the length of the sequence that should be printed.
+void fizz(printFizz) Calls printFizz to output "fizz".
+void buzz(printBuzz) Calls printBuzz to output "buzz".
+void fizzbuzz(printFizzBuzz) Calls printFizzBuzz to output "fizzbuzz".
+void number(printNumber) Calls printnumber to output the numbers.
+ 
+
+Example 1:
+
+Input: n = 15
+Output: [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz"]
+Example 2:
+
+Input: n = 5
+Output: [1,2,"fizz",4,"buzz"]
+ 
+
+Constraints:
+
+1 <= n <= 50
+ */
+public class _10_volatile_synchronized {
+
+    public static void main(String[] args) throws Exception {
+
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+
+        Thread t1 = new Thread(() -> {
+            try {
+                fizzBuzz.fizz(() -> System.out.print("fizz "));
+            } catch (InterruptedException e) {
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                fizzBuzz.buzz(() -> System.out.print("buzz "));
+            } catch (InterruptedException e) {
+            }
+        });
+
+        Thread t3 = new Thread(() -> {
+            try {
+                fizzBuzz.fizzbuzz(() -> System.out.print("fizzbuzz "));
+            } catch (InterruptedException e) {
+            }
+        });
+
+        Thread t4 = new Thread(() -> {
+            try {
+                fizzBuzz.number((x) -> System.out.print(x + " "));
+            } catch (InterruptedException e) {
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
     }
 }
