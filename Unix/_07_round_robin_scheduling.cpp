f603f359e71f7493c068e657a512bed2d7590b1b
SUHAIL SHARIEFF <149879419+Suhail-Sharieff@users.noreply.github.com>
2025-06-23 21:05:05 +0530

    Update _07_round_robin_scheduling.cpp

--- Unix/_07_round_robin_scheduling.cpp
+++ Unix/_07_round_robin_scheduling.cpp
@@ -1,110 +1,58 @@
-#include <iostream>
-#include <queue>
-#include <vector>
 #include<algorithm>
+#include<cstring>
+#include<cstdlib>
+#include<fstream>
+#include<fcntl.h>
+#include<iostream>
+#include<pthread.h>
+#include<semaphore.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<unistd.h>
+#include<vector>
+
 using namespace std;
 
-class Process {
-public:
-    int id;
-    int arrival_time;
-    int burst_time;
-    int remaining_time;
-    int completion_time;
-    int turnaround_time;
-    int waiting_time;
+int main(){
 
-    Process(int i) {
-        id = i;
-    }
+	int n=3;
+	int qt=2;
+	int at[]={0,1,2},bt[]={2,3,4},rt[]={2,3,4};//arrival,burst,remaining
 
-    void scanProcess() {
-        cout << "Enter arrival time for process " << id << ": ";
-        cin >> arrival_time;
-        cout << "Enter burst time for process " << id << ": ";
-        cin >> burst_time;
-        remaining_time = burst_time;
-    }
-};
-
-int main() {
-    int n, time_quantum;
-    cout << "Enter number of processes: ";
-    cin >> n;
-
-    vector<Process> processes;
-    for (int i = 0; i < n; ++i) {
-        processes.push_back(Process(i));
-        processes[i].scanProcess();
-    }
-
-    cout << "Enter time quantum: ";
-    cin >> time_quantum;
-
-    queue<int> q;
-    vector<bool> in_queue(n, false);
-    int timer = 0, completed = 0;
-
-    // Sort processes by arrival time
-    sort(processes.begin(), processes.end(), [](Process &a, Process &b) {
-        return a.arrival_time < b.arrival_time;
-    });
-
-    q.push(0);
-    in_queue[0] = true;
-
-    while (completed < n) {
-        if (q.empty()) {
-            // If queue is empty, move timer to next arrival
-            for (int i = 0; i < n; i++) {
-                if (processes[i].remaining_time > 0) {
-                    timer = max(timer, processes[i].arrival_time);
-                    q.push(i);
-                    in_queue[i] = true;
-                    break;
-                }
-            }
-            continue;
-        }
-
-        int i = q.front(); q.pop();
-        Process &p = processes[i];
-
-        int exec_time = min(time_quantum, p.remaining_time);
-        timer = max(timer, p.arrival_time);
-        timer += exec_time;
-        p.remaining_time -= exec_time;
-
-        // Check for new arrivals during execution
-        for (int j = 0; j < n; ++j) {
-            if (j != i && !in_queue[j] && processes[j].arrival_time <= timer && processes[j].remaining_time > 0) {
-                q.push(j);
-                in_queue[j] = true;
-            }
-        }
-
-        if (p.remaining_time > 0) {
-            q.push(i);  // Not yet finished
-        } else {
-            p.completion_time = timer;
-            p.turnaround_time = p.completion_time - p.arrival_time;
-            p.waiting_time = p.turnaround_time - p.burst_time;
-            completed++;
-        }
-    }
-
-    // Output results
-    cout << "\nProcess\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n";
-    long avgWttime=0l,avgTuraround=0l;
-    for (auto &p : processes) {
-        cout << "P" << p.id << "\t" << p.arrival_time << "\t" << p.burst_time << "\t"
-             << p.completion_time << "\t\t" << p.turnaround_time << "\t\t" << p.waiting_time << endl;
-             avgWttime+=p.waiting_time;
-             avgTuraround+=p.turnaround_time;
-    }
-    avgWttime/=n;
-    avgTuraround/=n;
-    cout<<"Avg waiting time: "<<avgWttime<<endl;
-    cout<<"Avg turnaround time "<<avgTuraround<<endl;
-    return 0;
+	int nCompleted=0;
+	int toavg_turnaround_timel_time=0;
+	int avg_turnaround_time=0;
+	int avg_waiting_time=0;
+	while(nCompleted<n){
+		for(int i=0;i<n;i++){
+			if(at[i]<=toavg_turnaround_timel_time){
+				if(rt[i]>0){
+					//its not yet completed
+					if(rt[i]>=qt){
+						toavg_turnaround_timel_time+=qt;
+						rt[i]-=qt;
+					}else{
+						toavg_turnaround_timel_time+=rt[i];
+						rt[i]=0;
+					}
+				}
+				if(rt[i]==0){
+					cout<<"Process "<<i
+					<<" Burst time: "<<bt[i]
+					<<" Turnaround time: "<<(toavg_turnaround_timel_time-at[i])
+					<<" Waiting time: "<<(toavg_turnaround_timel_time-bt[i]-at[i])<<endl;
+					nCompleted++;
+					avg_waiting_time+=(toavg_turnaround_timel_time-at[i]-bt[i]);
+					avg_turnaround_time+=(toavg_turnaround_timel_time-at[i]);
+					rt[i]=-1;
+				}
+			}
+		}
+	}
+	cout<<"Avg waiting "<<(avg_waiting_time/n)<<endl;
+	cout<<"Avg turnaround "<<(avg_turnaround_time/n)<<endl;
+	return 0;
 }
